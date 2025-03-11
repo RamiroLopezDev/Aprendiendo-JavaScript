47e4e0bc0c8daae7ace61a22e3b67e0455db89f5
RamiroLopezDev <ramiro.lopez.dev@gmail.com>
2025-03-11 12:05:54 -0300

    Add files via upload

---
+++ 00- Hello world.js
@@ -0,0 +1,15 @@
+// Esto es un comentario en JavaScript
+console.log("¡Hola, JavaScript!")
+
+/*Esto es un comentario de varias lineas
+en JavaScript*/
+console.log('¡Hola, JavaScript!')
+
+/*Para hacer un comentario de varias lineas de codigo debo seleccionar todas esas lineas
+y luego presionar shift + alt + a */
+
+
+/* console.log('¡Hola, JavaScript!')
+console.log('¡Hola, JavaScript!')
+console.log('¡Hola, JavaScript!')
+console.log('¡Hola, JavaScript!') */
