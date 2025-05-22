8125ace849aa1e96187049f1fda9fedede854e1d
RamiroLopezDev <ramiro.lopez.dev@gmail.com>
2025-05-22 20:55:44 -0300

    7mo commit: Primeros ejercicios en JavaScript

---
+++ 03-Begginers_exercises.js
@@ -0,0 +1,109 @@
+//Acá haremos ejercicios para principiantes
+
+// 1. Escribe un comentario en una linea
+
+// Este es un comentario de una linea
+
+// 2. Escribe un comentario de varias lineas
+
+/* Esto es un comentario
+de varias
+lineas */
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivo
+
+let nombre  = "Ramiro"
+let edad = 31
+let esEnfermero = true
+let variableIndefinida = undefined
+let variableNula = null
+let simbolo = Symbol("un simbolo")
+let numeroGrande = BigInt(432485239532432432)
+
+//4. Imprime por consola el valor de todas las variables
+
+console.log(nombre)
+console.log(edad)
+console.log(esEnfermero)
+console.log(variableIndefinida)
+console.log(variableNula)
+console.log(simbolo)
+console.log(numeroGrande)
+
+//5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof nombre)
+console.log(typeof edad)
+console.log(typeof esEnfermero)
+console.log(typeof variableIndefinida)
+console.log(typeof variableNula)
+console.log(typeof simbolo)
+console.log(typeof numeroGrande)
+
+//6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+nombre = "Martin"
+edad = 32
+esEnfermero = false
+variableIndefinida = undefined
+variableNula = "ahora no es nula"
+simbolo = Symbol("un cuadrado")
+numeroGrande = BigInt(3141516171819202122)
+
+console.log(nombre)
+console.log(edad)
+console.log(esEnfermero)
+console.log(variableIndefinida)
+console.log(variableNula)
+console.log(simbolo)
+console.log(numeroGrande)
+
+//7. A continuación, modifica los valores de las variables por otros de distinto tipo.
+
+nombre = true
+edad = "veinticinco"
+esEnfermero = 22
+variableIndefinida = 90
+variableNula = "tiene valor"
+simbolo = 43293254549865895389320342n
+numeroGrande = false
+
+console.log(nombre)
+console.log(edad)
+console.log(esEnfermero)
+console.log(variableIndefinida)
+console.log(variableNula)
+console.log(simbolo)
+console.log(numeroGrande)
+
+//8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+const nombreConst = "Ramiro"
+const edadConst = 31
+const esEnfermeroConst = true
+const variableIndefinidaConst = undefined
+const variableNulaConst = null
+const simboloConst = Symbol("un simbolo")
+const numeroGrandeConst = BigInt(432485239532432432)
+
+console.log(nombreConst)
+console.log(edadConst)
+console.log(esEnfermeroConst)
+console.log(variableIndefinidaConst)
+console.log(variableNulaConst)
+console.log(simboloConst)
+console.log(numeroGrandeConst)
+
+//9. A continuación, modifica los valores de las constantes.
+
+/* const nombreConst = "Juan"
+const edadConst = 23
+const esEnfermeroConst = false
+const variableIndefinidaConst = "no"
+const variableNulaConst = "tiene valor"
+const simboloConst = Symbol("triangulo")
+const numeroGrandeConst = BigInt(9999999999999999999999) */ 
+
+//10. Comenta las líneas que produzcan algún tipo de error al ejecutarse.
+
+//VIDEO 01:21HS
